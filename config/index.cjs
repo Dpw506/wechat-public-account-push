99797e18b8711762176cbd453d9e7f0b3fe2a91c
Dpw506 <81970166+Dpw506@users.noreply.github.com>
2023-02-13 23:59:28 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'okIOa6NqtnSdZgMmrJuBGv14Jiyw',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'GTPD0sAhth5x0VTTg5x2YeVj55DWEEUm6sBs8zdjY7Q',
+      useTemplateId: 'x7COIX2Xu9qKCc6wAI6QcMIGRNL-xK_YCa-iWHJ7je0',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '11-30',
       festivals: [
@@ -56,7 +56,7 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: 'GTPD0sAhth5x0VTTg5x2YeVj55DWEEUm6sBs8zdjY7Q',
+  CALLBACK_TEMPLATE_ID: 'x7COIX2Xu9qKCc6wAI6QcMIGRNL-xK_YCa-iWHJ7je0',
 
   CALLBACK_USERS: [
     {
